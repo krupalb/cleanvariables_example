d129c1777928f5bd4cf24bb82d601302873f7e27
krupalb <krupalb@gmail.com>
2023-04-28 12:57:54 +0100

    update script.sql

--- db_scripts/script.sql
+++ db_scripts/script.sql
@@ -1,8 +1,8 @@
 create table cleanVariables_users
 (
     id         int(6) unsigned auto_increment primary key,
-    name       varchar(100)                         not null,
-    email      varchar(100)                         not null,
-    password   varchar(255)                        not null,
+    name       varchar(100) not null,
+    email      varchar(100) not null,
+    password   varchar(255) not null,
     created_at timestamp default CURRENT_TIMESTAMP not null
 );
